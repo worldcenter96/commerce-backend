a7cdefeab391a27ac3e4b6a0b766d270ae70e2c2
hwk9407 <hwk__@naver.com>
2024-12-06 11:38:22 +0900

    [#20] feat: B2C 멤버가 조회할 수 없는 로직 추가
    - refactor: Dto 정적 팩토리 메서드 디자인 패턴 적용

--- b2c/src/main/java/com/sparta/b2c/product/service/ProductService.java
+++ b2c/src/main/java/com/sparta/b2c/product/service/ProductService.java
@@ -2,10 +2,14 @@ package com.sparta.b2c.product.service;
 
 import com.sparta.b2c.product.dto.response.ProductResponse;
 import com.sparta.impostor.commerce.backend.domain.product.entity.Product;
+import com.sparta.impostor.commerce.backend.domain.product.enums.ProductStatus;
 import com.sparta.impostor.commerce.backend.domain.product.repository.ProductRepository;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -16,16 +20,13 @@ public class ProductService {
     public ProductResponse retrieveProduct(Long id) {
         Product product = productRepository.findById(id)
                 .orElseThrow(() -> new EntityNotFoundException("상품을 찾지 못하였습니다."));
-        return new ProductResponse(
-                product.getId(),
-                product.getName(),
-                product.getDescription(),
-                product.getPrice(),
-                product.getStatus(),
-                product.getCategory(),
-                product.getSubCategory(),
-                product.getCreatedAt(),
-                product.getModifiedAt()
-        );
+        if (List.of(
+                ProductStatus.OFF_SALE,
+                ProductStatus.PENDING
+        ).contains(product.getStatus())) {
+            throw new AccessDeniedException("해당 상품은 조회할 수 없습니다.");
+        }
+
+        return ProductResponse.from(product);
     }
 }
