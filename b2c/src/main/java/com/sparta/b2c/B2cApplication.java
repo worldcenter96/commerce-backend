8a460ebf7632baef78f4402aff734463745d0d07
hwk9407 <hwk__@naver.com>
2024-12-07 23:04:11 +0900

    [#37] fix: application 클래스의 스캔범위를 적절하게 변경
    - 각 모듈별로 임시적으로 security 를 비활성화 하도록 설정

--- b2c/src/main/java/com/sparta/b2c/B2cApplication.java
+++ b2c/src/main/java/com/sparta/b2c/B2cApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@SpringBootApplication(scanBasePackages = "com.sparta.impostor.commerce.backend", exclude = { SecurityAutoConfiguration.class })
 public class B2cApplication {
 
     public static void main(String[] args) {
