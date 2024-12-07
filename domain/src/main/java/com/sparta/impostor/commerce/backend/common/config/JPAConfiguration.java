f2f90bfaf9f724ddddd3ad58fe7729ae90d18b00
hwk9407 <hwk__@naver.com>
2024-12-07 22:58:46 +0900

    [#37] fix: jpa 관련 설정 스캔 범위 설정 변경
    - 엔티티와 리포지토리를 좀 더 명확한 패키지를 스캔할 수 있도록 변경
    - Jpa Auditing 기능 활성화도 같이 추가

--- domain/src/main/java/com/sparta/impostor/commerce/backend/common/config/JPAConfiguration.java
+++ domain/src/main/java/com/sparta/impostor/commerce/backend/common/config/JPAConfiguration.java
@@ -6,11 +6,18 @@ import jakarta.persistence.PersistenceContext;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+/**
+ * @EntityScan, @EnableJpaRepositories 의 basePackages 는 @SpringBootApplication 의 scanBasePackages 의
+ * 범위 내에 포함되도록 설정했기 때문에 명시하지 않아도 동작하지만,
+ * 해당 패키지 내의 Repository와 Entity 클래스가 위치한 곳을 명확히 알려주기 위하여 추가하였습니다.
+ */
 @Configuration
-@EntityScan(basePackages = "com.sparta.impostor.commerce.backend")
-@EnableJpaRepositories(basePackages = "com.sparta.impostor.commerce.backend")
+@EnableJpaAuditing
+@EntityScan(basePackages = "com.sparta.impostor.commerce.backend.domain")
+@EnableJpaRepositories(basePackages = "com.sparta.impostor.commerce.backend.domain")
 public class JPAConfiguration {
 
     @PersistenceContext
