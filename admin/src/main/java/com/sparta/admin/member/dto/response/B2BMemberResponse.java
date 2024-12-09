672139b8cd099ab90656b832bfaf49209157192f
bbobsang <bbobsang@naver.com>
2024-12-09 19:26:43 +0900

    [#9] Build: 사용하지 않는 status 제거 ActiveStatus, RoleStatus

--- admin/src/main/java/com/sparta/admin/member/dto/response/B2BMemberResponse.java
+++ admin/src/main/java/com/sparta/admin/member/dto/response/B2BMemberResponse.java
@@ -6,16 +6,14 @@ public record B2BMemberResponse(
     Long id,
     String email,
     String name,
-    String activeStatus,
-    String roleStatus) {
+    String b2bMemberStatus) {
 
   public static B2BMemberResponse from(B2BMember member) {
     return new B2BMemberResponse(
         member.getId(),
         member.getEmail(),
         member.getName(),
-        member.getActiveStatus().name(),
-        member.getRoleStatus().name()
+        member.getB2BMemberStatus().name()
     );
   }
 }
