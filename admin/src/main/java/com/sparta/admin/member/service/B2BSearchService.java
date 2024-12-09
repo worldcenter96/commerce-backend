c2f9cadb053a366c4f87e76098be33d0561bb568
bbobsang <bbobsang@naver.com>
2024-12-09 23:20:45 +0900

    [#9] Feat: 예외처리 추가

--- admin/src/main/java/com/sparta/admin/member/service/B2BSearchService.java
+++ admin/src/main/java/com/sparta/admin/member/service/B2BSearchService.java
@@ -5,6 +5,7 @@ import com.sparta.impostor.commerce.backend.domain.b2bMember.entity.B2BMember;
 import com.sparta.impostor.commerce.backend.domain.b2bMember.enums.B2BMemberStatus;
 import com.sparta.impostor.commerce.backend.domain.b2bMember.repository.B2BMemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.boot.model.naming.IllegalIdentifierException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -25,7 +26,10 @@ public class B2BSearchService {
   }
 
   // 특정 status 애 해당하는 B2B 회원 조회 (status: INACTIVE, ACTIVE, PENDING)
-  public Page<B2BMemberResponse> getB2BMembersByStatus(B2BMemberStatus status, PageRequest pageRequest) {
+  public Page<B2BMemberResponse> getB2BMembersByStatus(
+      B2BMemberStatus status,
+      PageRequest pageRequest) {
+
     // 상태에 맞는 B2B 회원을 조회하여 Response 로 변환
     Page<B2BMember> b2bMemberPage = b2bMemberRepository.findByB2BMemberStatus(status, pageRequest);
 
@@ -36,7 +40,7 @@ public class B2BSearchService {
   public B2BMember updateB2BMemberStatus(Long b2bMemberId, B2BMemberStatus newStatus) {
 
     B2BMember b2bMember = b2bMemberRepository.findById(b2bMemberId)
-        .orElseThrow(() -> new RuntimeException("B2B Member Not Found"));
+        .orElseThrow(() -> new IllegalArgumentException("B2B 회원을 찾을 수 없습니다."));
   // 상태 변경
    b2bMember.setB2BMeberStatus(newStatus);
 
