0ede9848ad84a44bdd2e0d12ba69bf0757916d0b
bbobsang <bbobsang@naver.com>
2024-12-09 23:17:42 +0900

    [#9] Feat: sortBy, orderBy 제거

--- admin/src/main/java/com/sparta/admin/member/dto/response/B2BMemberPageResponse.java
+++ admin/src/main/java/com/sparta/admin/member/dto/response/B2BMemberPageResponse.java
@@ -1,12 +1,17 @@
 package com.sparta.admin.member.dto.response;
 
 import java.util.List;
+import org.springframework.data.domain.Page;
 
 public record B2BMemberPageResponse(
     List<B2BMemberResponse> members,
     int page,
-    int size,
-    String sortBy,
-    String orderBy,
-    int totalPages
-) {}
+    int size
+) {
+
+  public B2BMemberPageResponse(Page<B2BMemberResponse> pageResponse) {
+    this(pageResponse.getContent(),
+        pageResponse.getPageable().getPageNumber() + 1,
+        pageResponse.getPageable().getPageSize());
+  }
+}
