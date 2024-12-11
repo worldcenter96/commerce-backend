6f9dedee769df17b6504674dd735bc66f789a173
nurigaon96 <nurigaon96@gmail.com>
2024-12-11 18:09:28 +0900

    fix: B2B, B2C SearchController 주석 수정

--- admin/src/main/java/com/sparta/admin/member/controller/B2BSearchController.java
+++ admin/src/main/java/com/sparta/admin/member/controller/B2BSearchController.java
@@ -9,13 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/b2b-members")
