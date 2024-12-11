d5c7e78b4aa1e41b74356d0e2f6d4ea9f2f22914
bbobsang <bbobsang@naver.com>
2024-12-11 19:18:41 +0900

    [#7] Fix: 불필요한 코드 삭제 (status 추가하려했으나 취소)

--- domain/src/main/java/com/sparta/impostor/commerce/backend/domain/b2cMember/enums/B2CMemberStatus.java
+++ domain/src/main/java/com/sparta/impostor/commerce/backend/domain/b2cMember/enums/B2CMemberStatus.java
@@ -1,7 +1,6 @@
 package com.sparta.impostor.commerce.backend.domain.b2cMember.enums;
 
 public enum B2CMemberStatus {
-    PENDING, // 판매자 권한 신청 대기, 요청 확인을 위한 상태 추가
-    ACTIVE, // 판매자 권한 승인
-    INACTIVE // 판매자 권한 거절
+    ACTIVE,
+    INACTIVE,
 }
