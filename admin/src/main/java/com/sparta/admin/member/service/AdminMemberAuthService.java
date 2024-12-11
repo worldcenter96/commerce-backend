5c091d4482e17e65e69c80b0b619ca8b87331a5b
nurigaon96 <nurigaon96@gmail.com>
2024-12-11 16:25:49 +0900

    [#6] feat: Admin 로그인 기능 구현

--- admin/src/main/java/com/sparta/admin/member/service/AdminMemberAuthService.java
+++ admin/src/main/java/com/sparta/admin/member/service/AdminMemberAuthService.java
@@ -1,13 +1,21 @@
 package com.sparta.admin.member.service;
 
+import com.sparta.admin.member.dto.request.LoginRequest;
 import com.sparta.admin.member.dto.request.SignupRequest;
 import com.sparta.admin.member.dto.response.SignupResponse;
+import com.sparta.common.dto.MemberSession;
 import com.sparta.impostor.commerce.backend.domain.adminMember.entity.AdminMember;
 import com.sparta.impostor.commerce.backend.domain.adminMember.repository.AdminMemberRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.apache.catalina.util.StandardSessionIdGenerator;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.http.ResponseCookie;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.concurrent.TimeUnit;
 
 @Service
 @RequiredArgsConstructor
@@ -16,6 +24,7 @@ public class AdminMemberAuthService {
 
     private final AdminMemberRepository adminMemberRepository;
     private final BCryptPasswordEncoder passwordEncoder;
+    private final RedisTemplate<String, MemberSession> redisTemplate;
 
     public SignupResponse signup(SignupRequest request) {
 
@@ -31,5 +40,27 @@ public class AdminMemberAuthService {
         adminMemberRepository.save(createMember);
 
         return SignupResponse.from(createMember);
+    }
+
+    public ResponseCookie login(LoginRequest request) {
+        String email = request.email();
+        String rawPassword = request.password();
+
+        AdminMember member = adminMemberRepository.findByEmail(email).orElseThrow(() ->
+                new EntityNotFoundException("회원정보가 존재하지 않습니다."));
+
+        if (!passwordEncoder.matches(rawPassword, member.getPassword())) {
+            throw new IllegalArgumentException("패스워드가 일치하지 않습니다.");
+        }
+
+        String sessionId = new StandardSessionIdGenerator().generateSessionId();
+        redisTemplate.opsForValue().set(sessionId, new MemberSession(member.getId()), 30L, TimeUnit.MINUTES);
+
+        return ResponseCookie
+                .from("SESSION", sessionId)
+                .path("/")
+                .httpOnly(true)
+                .maxAge(1800)
+                .build();
     }
 }
