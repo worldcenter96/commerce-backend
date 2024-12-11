ae4aa76899ded57721887ee9375f0e64dba2a9d7
bbobsang <bbobsang@naver.com>
2024-12-11 09:38:00 +0900

    [#7] Fix: request 주입 및 ModelAttribute 임포트

--- admin/src/main/java/com/sparta/admin/member/controller/B2CSearchController.java
+++ admin/src/main/java/com/sparta/admin/member/controller/B2CSearchController.java
@@ -1,5 +1,6 @@
 package com.sparta.admin.member.controller;
 
+import com.sparta.admin.member.dto.request.B2CMemberSearchRequest;
 import com.sparta.admin.member.dto.response.B2CMemberPageResponse;
 import com.sparta.admin.member.service.B2CSearchService;
 import com.sparta.impostor.commerce.backend.domain.b2cMember.enums.B2CMemberStatus;
@@ -8,6 +9,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,21 +23,27 @@ public class B2CSearchController {
 
   // B2C 회원 전체 조회
   @GetMapping
-  public B2CMemberPageResponse getB2CMembers(
-      @RequestParam(defaultValue = "1") int page,
-      @RequestParam(defaultValue = "10") int size,
-      @RequestParam(defaultValue = "id") String sortBy,
-      @RequestParam(defaultValue = "asc") String orderBy,
-      @RequestParam(required = false) B2CMemberStatus status
-  ) {
-    Sort.Direction direction =
-        orderBy.equalsIgnoreCase("desc") ? Direction.DESC : Sort.Direction.ASC;
+  public B2CMemberPageResponse getB2CMembers(@ModelAttribute B2CMemberSearchRequest request) {
 
-    PageRequest pageRequest = PageRequest.of(
-        page - 1,
-        size,
-        Sort.by(direction, sortBy));
+    request.setDefaults();
 
-    return b2cSearchService.getB2CMembers(status, pageRequest);
+    // DTO 에서 값 추출
+    int page = request.getPage() - 1;
+    int size = request.getSize();
+    String sortBy = request.getSortBy();
+    String orderBy = request.getOrderBy();
+    B2CMemberStatus status = request.getStatus();
+
+    {
+      Sort.Direction direction =
+          orderBy.equalsIgnoreCase("desc") ? Direction.DESC : Sort.Direction.ASC;
+
+      PageRequest pageRequest = PageRequest.of(
+          page,
+          size,
+          Sort.by(direction, sortBy));
+
+      return b2cSearchService.getB2CMembers(status, pageRequest);
+    }
   }
 }
