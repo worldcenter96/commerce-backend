d40a8c537c291e6cf4b337db53575e637fe29f35
matino0216 <woqhr33@naver.com>
2024-12-19 17:40:53 +0900

    [#95]feat: init-s3.sh 지연 실행 수정

--- init-s3.sh
+++ init-s3.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+echo "Waiting for LocalStack to be ready..."
+until awslocal s3 ls > /dev/null 2>&1; do
+  echo "Waiting for S3 service..."
+  sleep 5
+done
+
 # S3 Bucket 생성
 awslocal s3 mb s3://local-bucket
+echo "S3 Bucket 'local-bucket' created"
