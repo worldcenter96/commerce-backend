3a8ff20791b28280a4353697e08d4abc6b1d7ca1
bbobsang <bbobsang@naver.com>
2024-12-24 12:30:24 +0900

    [#107] Fix: 특정 상태 B2C 회원 조회시, 전체 조회로 조회되는 이슈 해결

--- admin/src/main/java/com/sparta/admin/member/controller/B2CSearchController.java
+++ admin/src/main/java/com/sparta/admin/member/controller/B2CSearchController.java
@@ -1,18 +1,14 @@
 package com.sparta.admin.member.controller;
 
-import com.sparta.admin.member.dto.request.B2CMemberSearchRequest;
 import com.sparta.admin.member.dto.response.B2CMemberPageResponse;
 import com.sparta.admin.member.service.B2CSearchService;
 import com.sparta.common.annotation.CheckAuth;
 import com.sparta.common.enums.Role;
+import com.sparta.impostor.commerce.backend.domain.b2cMember.enums.B2CMemberStatus;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/admin/b2c-members")
@@ -21,20 +17,55 @@ public class B2CSearchController {
 
   private final B2CSearchService b2cSearchService;
 
-  // B2C 회원 전체 조회
+  /**
+   * B2C 회원 전체 조회
+   *
+   * @param page    현재 페이지 (기본값 1)
+   * @param size    페이지 당 사이즈 (기본값 10)
+   * @param sortBy  정렬 기준 (기본값 id)
+   * @param orderBy 정렬 방향 (기본값 asc)
+   * @return B2C 회원 목록
+   */
+
   @CheckAuth(role = Role.ADMIN)
-  @GetMapping
-  public B2CMemberPageResponse getB2CMembers(@ModelAttribute B2CMemberSearchRequest request) {
+  @GetMapping(produces = "application/json")
+  public ResponseEntity<B2CMemberPageResponse> getB2CMembers(
+          @RequestParam(required = false, defaultValue = "1") int page,
+          @RequestParam(required = false, defaultValue = "10") int size,
+          @RequestParam(required = false, defaultValue = "id") String sortBy,
+          @RequestParam(required = false, defaultValue = "asc") String orderBy
+  ) {
+    return ResponseEntity.status(HttpStatus.OK)
+            .body(b2cSearchService.getB2CMembers(page, size, sortBy, orderBy));
+  }
 
-    request.setDefaults();
 
-    // Pageable 생성
-    Sort.Direction direction =
-        request.getOrderBy().equalsIgnoreCase("desc") ? Sort.Direction.DESC : Sort.Direction.ASC;
 
-    Pageable pageable = PageRequest.of(request.getPage() - 1, request.getSize(),
-        Sort.by(direction, request.getSortBy()));
+  /**
+   * 특정 상태를 가진 B2C 회원 조회 (status: ACTIVE, INACTIVE)
+   *
+   * @param status  B2C 회원 상태 (ACTIVE, INACTIVE)
+   * @param page    현재 페이지 (기본값 1)
+   * @param size    페이지 당 사이즈 (기본값 10)
+   * @param sortBy  정렬 기준 (기본값 id)
+   * @param orderBy 정렬 방향 (기본값 asc)
+   * @return 특정 상태의 B2C 회원 목록
+   */
 
-    return b2cSearchService.getB2CMembers(request.getStatus(), pageable);
+  @CheckAuth(role = Role.ADMIN)
+  @GetMapping(value = "/status/{status}")
+  public ResponseEntity<B2CMemberPageResponse> getB2CMembersByStatus(
+          @PathVariable String status,
+          @RequestParam(required = false, defaultValue = "1") int page,
+          @RequestParam(required = false, defaultValue = "10") int size,
+          @RequestParam(required = false, defaultValue = "id") String sortBy,
+          @RequestParam(required = false, defaultValue = "asc") String orderBy
+  ) {
+
+    B2CMemberStatus b2cMemberStatus = B2CMemberStatus.valueOf(status.toUpperCase());
+
+    return ResponseEntity.status(HttpStatus.OK)
+            .body(b2cSearchService.getB2CMembersByStatus(page, size, sortBy, orderBy, b2cMemberStatus));
+
   }
 }
