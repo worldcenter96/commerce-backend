f4bb7ee13a74898c2c76b8495566e123a878e1a7
gebeen-dev <rrdd1207@gmail.com>
2024-12-31 16:25:38 +0900

    [#101] feat: 이미지파일 단건 업로드 Controller 생성
    필요없는 주석 삭제

--- b2b/src/main/java/com/sparta/b2b/fileUpload/controller/FileUploadController.java
+++ b2b/src/main/java/com/sparta/b2b/fileUpload/controller/FileUploadController.java
@@ -1,14 +1,16 @@
 package com.sparta.b2b.fileUpload.controller;
 
 import com.sparta.b2b.fileUpload.dto.ImageUploadedResponse;
+import com.sparta.b2b.fileUpload.dto.ImageUploadedResponseV2;
 import com.sparta.b2b.fileUpload.service.FileManageService;
 import com.sparta.common.annotation.CheckAuth;
+import com.sparta.common.annotation.LoginMember;
+import com.sparta.common.dto.MemberSession;
 import com.sparta.common.enums.Role;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -24,11 +26,21 @@ public class FileUploadController {
 	@Deprecated
 	@CheckAuth(role = Role.B2B)
 	@PostMapping("/products/images")
-	public ImageUploadedResponse uploadFiles(@RequestParam("images") List<MultipartFile> files) throws IOException {//images 갯수 제한(과금우려), 용량 제한 필수로 지정해야함
-		// 파일업로드 서비스로직
+	public ImageUploadedResponse uploadFiles(@RequestParam("images") List<MultipartFile> files) throws IOException {
+
 		ImageUploadedResponse apiResponse = fileUploadService.uploadFiles(files);
 		return apiResponse;
 	}
-	// 파일 업로드 api 호출 -> url 포함하여 상품등록 api 호출
-	// 상품 등록시 이미지 등록이 같이 되도록
+
+
+	@CheckAuth(role = Role.B2B)
+	@PostMapping("/products/image")
+	public ResponseEntity<ImageUploadedResponseV2> uploadFileV2(
+		@RequestParam("image") MultipartFile file,
+		@LoginMember(role = Role.B2B) MemberSession memberSession
+	) {
+		return ResponseEntity
+			.status(HttpStatus.CREATED)
+			.body(fileUploadService.uploadFileV2(file, memberSession.memberId()));
+	}
 }
