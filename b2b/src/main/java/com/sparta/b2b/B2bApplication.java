e05b02f4493da8d70de502f7cec59ccf0a1e7d75
gebeen-dev <rrdd1207@gmail.com>
2025-01-02 21:50:56 +0900

    [#114] feat: S3 고아 이미지 schedulering
    매월 3일 마다 삭제

--- b2b/src/main/java/com/sparta/b2b/B2bApplication.java
+++ b2b/src/main/java/com/sparta/b2b/B2bApplication.java
@@ -2,8 +2,10 @@ package com.sparta.b2b;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(scanBasePackages = {"com.sparta"})
+@EnableScheduling
 public class B2bApplication {
 
 	public static void main(String[] args) {
