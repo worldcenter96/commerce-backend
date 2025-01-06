622c206af764a6865001561bf09d7a6cf2d06b90
gebeen-dev <rrdd1207@gmail.com>
2025-01-06 18:34:06 +0900

    [#112] fix: 상품단건조회 수정
    본인의 상품만 조회 가능하도록 수정

--- domain/src/main/java/com/sparta/impostor/commerce/backend/domain/product/repository/ProductRepository.java
+++ domain/src/main/java/com/sparta/impostor/commerce/backend/domain/product/repository/ProductRepository.java
@@ -5,6 +5,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ProductRepository extends JpaRepository <Product, Long>, ProductRepositoryQuery {
 	Page<Product> findAllByMemberId(Long memberId, Pageable pageable);
+
+	Optional<Product> findByIdAndMemberId(Long productId, Long memberId);
 }
